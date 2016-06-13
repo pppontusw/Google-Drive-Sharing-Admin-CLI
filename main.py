0a9745135df67503619e2f92830ccaaea4bc329e
Pontus Wiberg <pontus@pontusw.com>
2016-06-13 11:14:30 +0200

    fix

--- main.py
+++ main.py
@@ -223,9 +223,10 @@ def buildItems(info):
 	except KeyError:
 		try:
 			logging.warning('Something wen\'t wrong when fetching items: %s ' % infolist)
+			return None
 		except UnboundLocalError:
 			logging.warning('Something wen\'t wrong when fetching items, info seems to be empty.'
-		return None
+			return None
 
 
 def makeQuery(drivesearchquery, user, token):
